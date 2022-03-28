efb0928ac8625182453deb7821fa04b4d9f69a17
David <david26694@gmail.com>
2022-03-28 19:10:38 +0200

    Data augmentation, safe cutoff

---
+++ models/hf_augment_predict.py
@@ -0,0 +1,35 @@
+
+# %% Load libraries and pipeline
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from transformers import pipeline
+
+model_path = Path("models") / "artifacts" / "hf" / "augment"
+data_path = Path("data")
+submission_path = Path("data") / "submission"
+final_name = "evaluation_public"
+model_name = "hf_augment"
+threshold = 0.42
+
+# Load pipeline
+p = pipeline("text-classification", model=str(model_path),
+             tokenizer=str(model_path))
+
+# %% Load data
+eval_df_raw = pd.read_csv(data_path / f"{final_name}.csv", delimiter="|")
+
+# %% Predict and build label
+scores = p(eval_df_raw.message.to_list(), return_all_scores=True)
+
+eval_df_raw["score"] = [x[1]["score"] for x in scores]
+eval_df_raw["label"] = np.where(
+    eval_df_raw["score"] > threshold, "racist", "non-racist")
+# %% Save data
+eval_df_raw.drop(columns=["score"]).to_csv(
+    submission_path / f"{model_name}.csv", index=False, sep="|")
+
+eval_df_raw.to_csv(submission_path / f"{model_name}_probs.csv", index=False)
+
+# %%
